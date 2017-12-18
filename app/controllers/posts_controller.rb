f548df2addbb3970da0e6c5acb26dab340a85a50
pbortnick <pbortnick@gmail.com>
2017-12-17 23:58:52 -0500

    added likes column to posts schema & controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -82,6 +82,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:title, :body, :avatar, :category_id)
+    params.require(:post).permit(:title, :body, :avatar, :category_id, :likes)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128215116) do
+ActiveRecord::Schema.define(version: 20171218012926) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171128215116) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer "category_id"
+    t.integer "likes"
   end
 
 end
